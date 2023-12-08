3a6ce21e79b5e7682706ee2e390dd886f9c4d9cd
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-12-09 00:22:36 +0530

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,16 +14,16 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://subhadipbhowmik.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/30-Days-Of-CPP",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
-
+  organizationName: "subhadipbhowmik", // Usually your GitHub org/user name.
+  projectName: "30-Days-Of-CPP", // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
