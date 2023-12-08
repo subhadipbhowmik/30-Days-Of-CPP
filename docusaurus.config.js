a751767a6121a18df9ec8bd434d08a774b8caa7e
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-12-09 02:06:22 +0530

    fix: change edit urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,14 +45,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/subhadipbhowmik/30-Days-Of-CPP/tree/main/docs/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/subhadipbhowmik/30-Days-Of-CPP/tree/main/blog/",
         },
         theme: {
           customCss: "./src/css/custom.css",
