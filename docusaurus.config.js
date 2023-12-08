392b013fa78b95a3ae7288df84048a3905f987cd
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-12-09 02:17:04 +0530

    fix: githublink update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/subhadipbhowmik/30-Days-Of-CPP/",
             label: "GitHub",
             position: "right",
           },
@@ -125,7 +125,7 @@ const config = {
               },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/subhadipbhowmik/30-Days-Of-CPP/",
               },
             ],
           },
