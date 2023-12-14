0f716e80faefc74ee6e3acdad3639c1727792bd1
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-12-15 00:21:59 +0530

    update: 🐛 fixed all live algolia search with crawler

---
+++ siteConfig.js
@@ -0,0 +1,103 @@
+new Crawler({
+  rateLimit: 8,
+  maxDepth: 10,
+  maxUrls: 5000,
+  startUrls: [
+    "https://subhadipbhowmik.github.io/30-Days-Of-CPP/",
+    "https://subhadipbhowmik.github.io/30-Days-Of-CPP/docs/",
+  ],
+  renderJavaScript: false,
+  sitemaps: ["https://subhadipbhowmik.github.io/30-Days-Of-CPP/sitemap.xml"],
+  ignoreCanonicalTo: false,
+  discoveryPatterns: [
+    "https://subhadipbhowmik.github.io/30-Days-Of-CPP/docs/**",
+  ],
+  schedule: "at 19:12 on Tuesday",
+  actions: [
+    {
+      indexName: "subhadipbhowmik",
+      pathsToMatch: [
+        "https://subhadipbhowmik.github.io/30-Days-Of-CPP/**",
+        "https://subhadipbhowmik.github.io/30-Days-Of-CPP/docs/**",
+      ],
+      recordExtractor: ({ helpers }) => {
+        return helpers.docsearch({
+          recordProps: {
+            lvl1: ["header h1", "article h1", "main h1", "h1", "head > title"],
+            content: ["article p, article li", "main p, main li", "p, li"],
+            lvl0: {
+              selectors: "",
+              defaultValue: "Documentation",
+            },
+            lvl2: ["article h2", "main h2", "h2"],
+            lvl3: ["article h3", "main h3", "h3"],
+            lvl4: ["article h4", "main h4", "h4"],
+            lvl5: ["article h5", "main h5", "h5"],
+            lvl6: ["article h6", "main h6", "h6"],
+          },
+          aggregateContent: true,
+          recordVersion: "v3",
+        });
+      },
+    },
+  ],
+  safetyChecks: { beforeIndexPublishing: { maxLostRecordsPercentage: 30 } },
+  initialIndexSettings: {
+    subhadipbhowmik: {
+      attributesForFaceting: ["type", "lang"],
+      attributesToRetrieve: [
+        "hierarchy",
+        "content",
+        "anchor",
+        "url",
+        "url_without_anchor",
+        "type",
+      ],
+      attributesToHighlight: ["hierarchy", "content"],
+      attributesToSnippet: ["content:10"],
+      camelCaseAttributes: ["hierarchy", "content"],
+      searchableAttributes: [
+        "unordered(hierarchy.lvl0)",
+        "unordered(hierarchy.lvl1)",
+        "unordered(hierarchy.lvl2)",
+        "unordered(hierarchy.lvl3)",
+        "unordered(hierarchy.lvl4)",
+        "unordered(hierarchy.lvl5)",
+        "unordered(hierarchy.lvl6)",
+        "content",
+      ],
+      distinct: true,
+      attributeForDistinct: "url",
+      customRanking: [
+        "desc(weight.pageRank)",
+        "desc(weight.level)",
+        "asc(weight.position)",
+      ],
+      ranking: [
+        "words",
+        "filters",
+        "typo",
+        "attribute",
+        "proximity",
+        "exact",
+        "custom",
+      ],
+      highlightPreTag: '<span class="algolia-docsearch-suggestion--highlight">',
+      highlightPostTag: "</span>",
+      minWordSizefor1Typo: 3,
+      minWordSizefor2Typos: 7,
+      allowTyposOnNumericTokens: false,
+      minProximity: 1,
+      ignorePlurals: true,
+      advancedSyntax: true,
+      attributeCriteriaComputedByMinProximity: true,
+      removeWordsIfNoResults: "allOptional",
+    },
+  },
+  appId: "SKRAQTXG41",
+  apiKey: "5429a3e02c9143e12ea148b0ab9e9ac4",
+  extraUrls: [
+    "https://subhadipbhowmik.github.io/30-Days-Of-CPP/docs/intro",
+    "https://subhadipbhowmik.github.io/30-Days-Of-CPP/blog",
+  ],
+});
