0b8aa49cc26d74bd9018f9dc31d54c1ede14974d
Eiva <eivayadav@gmail.com>
2024-05-31 01:34:28 +0530

    added AI Chatbot

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Chatbot from "../components/Chatbot/chatbot";
 import Heading from "@theme/Heading";
 import styles from "./index.module.css";
 
@@ -37,6 +38,7 @@ export default function Home() {
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        <Chatbot />
       </main>
     </Layout>
   );
