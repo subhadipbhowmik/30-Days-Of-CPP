986787c1e8185713da99ac89338c5b8cc891de82
srisanth seth <84568590+sri766@users.noreply.github.com>
2024-06-01 09:42:49 +0530

    added custom styles to hero section (#84)

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,10 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
+        <Heading as="h1" className={clsx("hero__title", styles.main_title)}>
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className={clsx("hero__subtitle",styles.sub_title)}>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
