5de06f0546bc9b9704ac5460bdece46c8501c89e
Sayantan_Karmakar <skarmakar1107@gmail.com>
2024-06-09 11:16:11 +0530

    added navbar to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,42 @@
 import Link from '@docusaurus/Link';
 import React, { useState } from 'react';
-
 <link rel="stylesheet" type="text/css" href="/css/custom.css"></link>
 
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Chatbot from "../components/Chatbot/chatbot";
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
 
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary")} style={{ display:'none' }}>
+      <div className="container">
+        <Heading as="h1" className={clsx("hero__title")}>
+        </Heading>
+      </div>
+    </header>
+  );
+}
 
 function App() {
+  const { siteConfig } = useDocusaurusContext();
   const [passwordShown, setPasswordShown] = useState(false);
-
   const togglePasswordVisibility = () => {
     setPasswordShown(!passwordShown);
   };
-
   return (
-    
-    <div className="form-container">
+    <>
+     <Layout
+      title="Home"
+      description="30 days of CPP programming challenge is a step-by-step guide to learn CPP programming language in 30 days. Master the fundamental concepts of CPP Programming easily."
+    >
+      <HomepageHeader />
+      <main style={{ position: 'relative' }}>
+        <div className="form-container"> 
         <h2>Login</h2>
       <form>
         <div className="input-container">
@@ -41,6 +63,10 @@ function App() {
         </div>
       </form>
     </div>
+        <Chatbot />
+      </main>
+    </Layout>
+    </>
   );
 }
 
