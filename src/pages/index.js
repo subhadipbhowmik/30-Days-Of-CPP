ebdd7a31405d8f3b552b09ff6a7aeb4669f8fb93
bristiHalder <22cd3009@rgipt.ac.in>
2024-06-10 18:25:35 +0530

    Added Daily Tips Feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
+import React from 'react';
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Chatbot from "../components/Chatbot/chatbot";
 import Heading from "@theme/Heading";
 import styles from "./index.module.css";
+import DailyTips from "../components/DailyTips/DailyTips"; 
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -15,7 +17,7 @@ function HomepageHeader() {
         <Heading as="h1" className={clsx("hero__title", styles.main_title)}>
           {siteConfig.title}
         </Heading>
-        <p className={clsx("hero__subtitle",styles.sub_title)}>{siteConfig.tagline}</p>
+        <p className={clsx("hero__subtitle", styles.sub_title)}>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -28,6 +30,7 @@ function HomepageHeader() {
     </header>
   );
 }
+
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -37,6 +40,7 @@ export default function Home() {
     >
       <HomepageHeader />
       <main style={{ position: 'relative' }}>
+        <DailyTips /> 
         <HomepageFeatures />
         <Chatbot />
       </main>
