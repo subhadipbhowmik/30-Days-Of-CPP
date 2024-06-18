e311b8c098a2c6af81d570e2d42e015666612450
Vipul_Lakum <lakumvipul6351@gmail.com>
2024-06-18 14:01:41 +0530

    Problem in navabr added

---
+++ src/components/Table/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Tags from "../Tags";
+
+export default function Table({
+  title,
+  collectionLink,
+  isSorted = true,
+  data,
+}) {
+  const hasTopic = data.some(d => d.tags);
+  const sortOrder = {
+    Easy: 0,
+    Medium: 1, 
+    Hard: 2,
+  };
+  const renderDifficultyClassName = (difficulty) => {
+    return difficulty.toLowerCase();
+  };
+  const renderRow = (
+    isSorted
+      ? data.sort((x, y) => sortOrder[x.difficulty] - sortOrder[y.difficulty])
+      : data
+  ).map((d, idx) => {
+    return (
+      <tr key={idx}>
+        <td>
+          <a href={d.leetCodeLink} target="_blank" rel="noopener noreferrer">
+            {d.problemName}
+          </a>
+        </td>
+        <td className={renderDifficultyClassName(d.difficulty)}>
+          {d.difficulty}
+        </td>
+        <td style={{ textAlign: "center" }}>
+          {d.solutionLink ? (
+            <a href={d.solutionLink} target="_blank" rel="noopener noreferrer">
+              View Solutions
+            </a>
+          ) : (
+            <span>N/A</span>
+          )}
+        </td>
+        {hasTopic && (
+          <td>
+            <Tags names={d.tags} />
+          </td>
+        )}
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <h3>{title}</h3>
+      {collectionLink && (
+        <h4>
+          Start Practicing:{" "}
+          <a href={collectionLink} target="_blank" rel="noopener noreferrer">
+            {collectionLink}
+          </a>
+        </h4>
+      )}
+      <table className="suggested-problem-table">
+        <thead>
+          <tr>
+            <th>Problem Name</th>
+            <th>Difficulty</th>
+            <th>Solution Link</th>
+            {hasTopic && <th>Topic</th>}
+          </tr>
+        </thead>
+        <tbody>{renderRow}</tbody>
+      </table>
+    </>
+  );
+}
