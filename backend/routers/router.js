6b933a4345cf96579decb774a7b9abf02cf150ec
Shubhadip Bhowmik <shubhadipbhowmik02@gmail.com>
2024-06-25 09:22:24 +0530

    Restructuring The Codebase

--- backend/routers/router.js
+++
@@ -1,8 +0,0 @@
-// routes/router.js
-const express = require('express');
-const router = express.Router();
-const authRoutes = require('./auth');
-
-router.use('/auth', authRoutes);
-
-module.exports = router;
--- sidebars.js
+++ sidebars.js
@@ -28,7 +28,6 @@ const sidebars = {
     },
   ],
    */
-
 };
 
 export default sidebars;
